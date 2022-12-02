3ca521e588408aee6f88b74e4d33370943dfbafb
hikinine <paulo.artlab@gmail.com>
2022-12-02 10:06:36 -0300

    exports module

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,5 +2,7 @@ export * from "./Controller"
 export * from "./Documentation"
 export * from "./Interval"
 export * from "./Listener"
+export * from "./Module"
 export * from "./Provider"
 export * from "./Repository"
+
