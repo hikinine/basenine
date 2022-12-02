7ce908efbc93577e7965bbf9f8de17e89532b1da
hikinine <paulo.artlab@gmail.com>
2022-12-02 10:31:35 -0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./abstract";
 export { ApplicationContainer } from './container';
 export * from "./decorators";
+export * from "./errors";
+export * from "./interface/entity";
 export { ExpressHttpServer } from './server.express';
 
